9c2b3a51aa6983107c367d27a9a735ec598e8e7a
thehouseplant <sean.collins@outlook.com>
2018-04-09 22:21:05 -0700

    Added better data cleaning and pass at CSV parsing

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
+const _ = require('lodash');
+const dataset = require('./data/dataset.json');
 const fs = require('fs');
 const Json2csvParser = require('json2csv').Parser;
-const dataset = require('./data/data.json');
 
+let chosenQuestions = [
+    'ID','country','survey', 'Q2', 'q3', 'Q4', 'Q5', 'Q6', 'Q6N', 'Q7', 'Q23', 'Q24A', 'Q24B', 'Q27A', 'Q27B', 
+    'Q27C', 'Q27D', 'Q27E', 'Q27F', 'Q30', 'Q46a', 'Q46b', 'Q46c', 'q56', 'Q85A', 'Q85B', 'Q85C', 'Q85D', 'q100us'
+];
 let finalData = [];
 
 for (let i = 0; i < dataset.length; i++) {
     let row = dataset[i];
+
     // China is 3 and United States is 19
     if (row.country == 19 || row.country == 3) {
-        finalData.push(row);
+        let cleanedRow = _.pick(row, chosenQuestions);
+        finalData.push(cleanedRow);
     }
 }
-
-console.log(`New data length: ${finalData.length}`);
 
 fs.writeFile('./finalData.json', JSON.stringify(finalData), (err) => {
     if (err) {
         console.error(err);
         return;
     }
-    console.log('New dataset created');
+    console.log(`New dataset created with length of ${finalData.length} rows`);
 });
 
+/*
 try {
     const parser = new Json2csvParser(opts);
     const csv = parser.parse(finalData);
@@ -29,3 +35,4 @@ try {
 } catch (err) {
     console.error(err);
 }
+*/
